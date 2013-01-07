3ea88586e649c52f8e0e629111d1cdd91f245efe
Paul Schorfheide <paul@noveltyplant.com>
2013-01-07 10:33:58 -0500

    test query

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return 'Hello World!'
+  conn = psycopg2.connect(os.environ.get(HEROKU_POSTGRESQL_TEAL_URL))
+  cur = conn.cursor()
+  cur.execute('SELECT * from guests')
+  return str(cur.fetchall())
 
 if __name__ == '__main__':
   # Bind to PORT if defined, otherwise default to 5000.
